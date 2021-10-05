928c6d80d161ddc80b84520b79e93523d9ace98e
Youssef Tanzekhti <tanzekhtiyoussef60@yahoo.com>
2021-10-06 00:35:25 +0200

    worked

--- src/portfolio.js
+++ src/portfolio.js
@@ -239,7 +239,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
